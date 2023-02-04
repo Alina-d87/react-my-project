ed721cee1992733aa530c7170e525b874f7ab4ca
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2023-02-04 19:45:50 +0200

    add...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,30 +5,30 @@
 //import About from "path/to/pages/About";
 //import Products from "path/to/pages/Products";
 
-//import Header from "styled-components"
+import Header from "styled-components"
 
-//export const Header = () => {
-//	return (
-//		<Header>
-//			<Logo>pe<span>t</span>ly</Logo>
-//			<Navigation>
-//				//Загальна навігація
-//				<nav>
-//<Link to="/">Home</Link>
-//  <Link to="/new">New</Link>
-//  <Link to="/find pet">Find pet</Link>
-//  <Link to="/our friends">Our friends</Link>
-//</nav>
-//			//навігація для авторизованого користувача
-//				<UserNav>
-//					<button>Login</button>
-//					<button>Registration</button>
-//				</UserNav>
-//				//навігація для НЕавторизованого користувача
-//				<Authnav>
-//					<button>Account</button>
-//				</Authnav>
-//			</Navigation>
-//		</Header>
-//	)
-//}
+export const Header = () => {
+	return (
+		<Header>
+			<Logo>pe<span>t</span>ly</Logo>
+			<Navigation>
+				//Загальна навігація
+				<nav>
+<Link to="/">Home</Link>
+  <Link to="/new">New</Link>
+  <Link to="/find pet">Find pet</Link>
+  <Link to="/our friends">Our friends</Link>
+</nav>
+			//навігація для авторизованого користувача
+				<UserNav>
+					<button>Login</button>
+					<button>Registration</button>
+				</UserNav>
+				//навігація для НЕавторизованого користувача
+				<Authnav>
+					<button>Account</button>
+				</Authnav>
+			</Navigation>
+		</Header>
+	)
+}
